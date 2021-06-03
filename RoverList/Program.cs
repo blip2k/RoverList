d4c73cb0ae12ca5608f6faa7860939a067fdf711
blair <blairharris360@gmail.com>
2021-06-03 07:51:11 -0400

    finsihed

--- RoverList/Program.cs
+++ RoverList/Program.cs
@@ -12,6 +12,74 @@ namespace RoverList
         {
             RoverList<string> list = new RoverList<string>();
 
+            list.Add("blip");
+            list.Add("clip");
+            list.Add("lip");
+            list.Add("hip");
+            list.Add("tip");
+            list.Add("rip");
+            list.Add("flip");
+            list.Add("slip");
+            list.Add("pip");
+            list.Add("whip");
+            list.Add("zip");
+            list.Add("grip");
+            list.Add("chip");
+            list.Add("snip");
+            list.Add("drip");
+            list.Add("skip");
+
+            list.ListNodes();
+
+            Console.WriteLine("Type in a word then type done when you have done so");
+            String word = "";
+            while (word != "done")
+            {
+                if (word == "done")
+                {
+                    break;
+                }
+                else
+                {
+                    list.Add(word);
+                    word = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Type in a word at the front of the list now. Type done when finished.");
+            word = "";
+            while (word != "done")
+            {
+                if (word == "done")
+                {
+                    break;
+                }
+                else
+                {
+                    list.Add(0, word);
+                    word = Console.ReadLine();
+                }
+            }
+            list.ListNodes();
+
+            for (int i = 0; i < list.toCount() - 2; i++)
+            {
+                string d = list.ElementAt(i);
+                if (list.ElementAt(i) != null)
+                {
+
+                    int length = d.Length;
+
+                    if (length % 2 == 1)
+                    {
+                        list.RemoveAt(i);
+                    }
+                }
+            }
+            list.ListNodes();
+
+            list.Clear();
+            list.ListNodes();
             // TODO:  Implement the RoverList class
             // TODO:  Create a RoverList and then fill it with 16 words
 
--- RoverList/RoverList.cs
+++ RoverList/RoverList.cs
@@ -9,42 +9,142 @@ namespace RoverList
     public class RoverList<T> : RoverListBase<T>
     {
         // Add any variables you need here
-        
-        public RoverList ()
-        {
+        public int counter;
 
+
+        public RoverList()
+        {
+            counter = 0;
         }
 
-        public override int Count => throw new NotImplementedException();
-
+        public int toCounter()
+        {
+            current = head;
+            if (head == null)
+            {
+                counter = 0;
+            }
+            while (current != null)
+            {
+                counter++;
+                current = current.Next;
+            }
+            return counter;
+        }
+        public override int Counter
+        {
+            get
+            {
+                return counter;
+            }
+        }
         public override void Add(T data)
         {
-            throw new NotImplementedException();
+            Node newNode = new Node(data);
+            Node current = head;
+            if (head == null)
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+            else
+            {
+                while (current != null)
+                {
+                    if (current.Next == null)
+                    {
+                        current.Next = newNode;
+                        break;
+                    }
+                    current = current.Next;
+                }
+            }
         }
 
         public override void Add(int Position, T data)
         {
-            throw new NotImplementedException();
+            int pos = 0;
+            Node current = head;
+            Node newNode = new Node(data);
+            if (Position == 0)
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+            else
+            {
+                while (current != null)
+                {
+                    if (pos == Position)
+                    {
+                        current.Next = newNode;
+                        break;
+                    }
+                    current = current.Next;
+                    pos++;
+                }
+            }
         }
 
         public override void Clear()
         {
-            throw new NotImplementedException();
+            head = null;
+            count = 0;
         }
 
         public override T ElementAt(int Position)
         {
-            throw new NotImplementedException();
+            Node current = head;
+            int pos = 0;
+            while (current != null)
+            {
+                if (pos == Position)
+                {
+                    return current.Data;
+                }
+                current = current.Next;
+                pos++;
+            }
+            return default(T);
         }
 
         public override void ListNodes()
         {
-            throw new NotImplementedException();
+            Node current = head;
+            while (current != null)
+            {
+
+                Console.WriteLine(current.Data + " ,");
+                current = current.Next;
+
+            }
         }
 
         public override bool RemoveAt(int Position)
         {
-            throw new NotImplementedException();
+            Node current = head;
+            bool removed = false;
+            int pos = 0;
+            if (Position == 0)
+            {
+                head = head.Next;
+                removed = true;
+            }
+            else
+            {
+                while (current != null)
+                {
+                    if (pos == Position)
+                    {
+                        current.Next = current.Next.Next;
+                        removed = true;
+                        break;
+                    }
+                    current = current.Next;
+                    pos++;
+                }
+            }
+            return removed;
         }
     }
 }
